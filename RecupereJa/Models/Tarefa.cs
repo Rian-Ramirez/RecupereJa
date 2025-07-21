c5ec271b6d135184f4e78c20bac3b94c98cb222a
Rian <rianarthurcomercial@gmail.com>
2025-07-21 20:20:45 -0300

    Envio das tarefas

---
+++ RecupereJa/Models/Tarefa.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using RecupereJa.Enums;
+using RecupereJa.Services;
+using RecupereJa.ViewModels;
+
+namespace RecupereJa.Models
+{
+    public class Tarefa
+    {
+        public Tarefa()
+        {
+            DataCriacao = DateTime.Now;
+            Concluida = false;
+            Prioridade = PrioridadeEnum.Media;
+        }
+
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public DateTime? DataConclusao { get; set; }
+
+        public bool Concluida { get; set; }
+
+        public PrioridadeEnum Prioridade { get; set; }  
+    }
+}
---
+++ RecupereJa/Services/ITarefaService.cs
@@ -0,0 +1,10 @@
+﻿using RecupereJa.Models;
+using RecupereJa.Repository;
+
+namespace RecupereJa.Services
+{
+    public interface ITarefaService : ICRUD<Tarefa>
+    {
+        public Task<List<Tarefa>> BuscarOrdenadoDataCriacaoDesc();
+    }
+}
