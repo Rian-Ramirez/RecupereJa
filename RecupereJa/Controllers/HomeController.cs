92ab1a46726b020064050d816a568ace2983a20c
Rian <rianarthurcomercial@gmail.com>
2025-07-21 16:07:05 -0300

    Alterações sla o que

--- RecupereJa/Controllers/HomeController.cs
+++ RecupereJa/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace RecupereJa.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
