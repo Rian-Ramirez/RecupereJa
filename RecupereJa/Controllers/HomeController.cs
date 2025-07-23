caff205bd35a4d42d33479aa87108766ce5b87b8
Rian <rianarthurcomercial@gmail.com>
2025-07-22 21:10:48 -0300

    inserição do banco de dados e auatorização sla

--- RecupereJa/Controllers/HomeController.cs
+++ RecupereJa/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RecupereJa.Models;
 
 namespace RecupereJa.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
