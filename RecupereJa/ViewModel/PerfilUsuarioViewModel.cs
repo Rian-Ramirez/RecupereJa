bd7529dc8f8fd49dec58a27b77de7eeef11a5e8c
Rian <rianarthurcomercial@gmail.com>
2025-08-05 19:39:30 -0300

    HDSOUASDUAGSUDGAUSGDUSA

--- RecupereJa/ViewModel/PerfilUsuarioViewModel.cs
+++ RecupereJa/ViewModel/PerfilUsuarioViewModel.cs
@@ -2,5 +2,28 @@
 {
     public class UsuarioViewModel
     {
+            public string Name { get; set; }
+            public string Position { get; set; }
+            public string Location { get; set; }
+            public string PhotoUrl { get; set; }
+            public double Rating { get; set; }
+
+            public string Phone { get; set; }
+            public string Email { get; set; }
+            public string Website { get; set; }
+            public string Address { get; set; }
+
+            public DateTime Birthday { get; set; }
+            public string Gender { get; set; }
+
+            public List<string> Skills { get; set; }
+            public List<Workplace> Workplaces { get; set; }
+
+        public class Workplace
+        {
+            public string Company { get; set; }
+            public string Address { get; set; }
+        }
+
     }
 }
