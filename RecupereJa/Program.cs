3ec98ade0ba1ea0774f931ad0a4c3aadb5366739
Rian <rianarthurcomercial@gmail.com>
2025-08-26 20:09:44 -0300

    Correção dos Cookies

--- RecupereJa/Program.cs
+++ RecupereJa/Program.cs
@@ -9,15 +9,21 @@ using Auth0.AspNetCore.Authentication;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddAuth0WebAppAuthentication(options => {
-        options.Domain = builder.Configuration["Auth0:dev-nkkhmfsoi4d1o532.us.auth0.com"];
-        options.ClientId = builder.Configuration["Auth0:qMnhpdKd3NMx0WI3cLZqCZEfBeaTl3tz"];
+    .AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Domain"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
     });
 
 // Configuração do DbContext com Pomelo MySQL
 builder.Services.AddDbContext<RecupereJaContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-    new MySqlServerVersion(new Version(8, 0, 39)))
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 39)),
+        mySqlOptions => mySqlOptions.EnableRetryOnFailure()
+    )
+    .EnableSensitiveDataLogging()
+    .EnableDetailedErrors()
 );
 
 
@@ -28,15 +34,14 @@ builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(o =>
-    {
-        o.LoginPath = "/Usuario/Login";
-        o.AccessDeniedPath = "/Usuario/AcessoNegado";
-        o.SlidingExpiration = true;
-        o.Cookie.HttpOnly = true;
-        o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    });
+builder.Services.ConfigureApplicationCookie(o =>
+{
+    o.LoginPath = "/Usuario/Login";
+    o.AccessDeniedPath = "/Usuario/AcessoNegado";
+    o.SlidingExpiration = true;
+    o.Cookie.HttpOnly = true;
+    o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
 
 builder.Services.AddControllersWithViews();
 
