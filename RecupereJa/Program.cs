109ae02a754f92c80a8ffd96001755d4d9f46adb
Rian <rianarthurcomercial@gmail.com>
2025-08-27 17:05:36 -0300

    Remoção Auth0

--- RecupereJa/Program.cs
+++ RecupereJa/Program.cs
@@ -1,47 +1,33 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
+ï»¿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using RecupereJa.Data;
 using RecupereJa.Repository;
 using RecupereJa.Repositorio;
 using RecupereJa.Services;
-using Auth0.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddAuth0WebAppAuthentication(options =>
-    {
-        options.Domain = builder.Configuration["Auth0:Domain"];
-        options.ClientId = builder.Configuration["Auth0:ClientId"];
-    });
-
-// Configuração do DbContext com Pomelo MySQL
+// Configuraï¿½ï¿½o do DbContext com Pomelo MySQL
 builder.Services.AddDbContext<RecupereJaContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 39)),
-        mySqlOptions => mySqlOptions.EnableRetryOnFailure()
-    )
-    .EnableSensitiveDataLogging()
-    .EnableDetailedErrors()
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 39)))
 );
 
-
-
-// Injeções de dependência
+// Injeï¿½ï¿½es de dependï¿½ncia
 builder.Services.AddScoped<IItemRepositorio, ItemRepositorio>();
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 
-builder.Services.ConfigureApplicationCookie(o =>
-{
-    o.LoginPath = "/Usuario/Login";
-    o.AccessDeniedPath = "/Usuario/AcessoNegado";
-    o.SlidingExpiration = true;
-    o.Cookie.HttpOnly = true;
-    o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-});
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/Usuario/Login";
+        o.AccessDeniedPath = "/Usuario/AcessoNegado";
+        o.SlidingExpiration = true;
+        o.Cookie.HttpOnly = true;
+        o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    });
 
 builder.Services.AddControllersWithViews();
 
--- RecupereJa/Services/ItemService.cs
+++ RecupereJa/Services/ItemService.cs
@@ -13,10 +13,10 @@ namespace RecupereJa.Services
 
         public Task<Item> CriarAsync(Item entidade) => _iitemRepositorio.CriarAsync(entidade);
         public Task<Item?> BuscarPorIdAsync(int id) => _iitemRepositorio.BuscarPorIdAsync(id);
-        public Task<System.Collections.Generic.List<Item>> BuscarTodosAsync() => _iitemRepositorio.BuscarTodosAsync();
         public Task<Item> AtualizarAsync(Item entidade) => (Task<Item>)_iitemRepositorio.AtualizarAsync(entidade);
         public Task<bool> DeletarAsync(int id) => _iitemRepositorio.DeletarAsync(id);
 
+        public Task<System.Collections.Generic.List<Item>> BuscarTodosAsync() => _iitemRepositorio.BuscarTodosAsync();
         public Task<System.Collections.Generic.List<Item>> BuscarOrdenadoDataCriacaoDescAsync() => _iitemRepositorio.BuscarOrdenadoDataCriacaoDescAsync();
         public Task<System.Collections.Generic.List<Item>> BuscarItemParaHomeAsync() => _iitemRepositorio.BuscarItemParaHomeAsync();
     }
