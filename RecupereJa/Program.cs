ab2f3798e007c8d85f5e2c7d49fd9d91dfec263b
MaiconGabrielVanelli <maicongabrielvanelli@gmail.com>
2025-08-28 19:34:16 -0300

    Altera rota padrão para controlador Home e ação Index
    A rota padrão definida no método `app.MapControllerRoute` no
    arquivo `Program.cs` foi modificada. Antes, o controlador
    padrão era `Usuario` e a ação padrão era `Login`. Agora, o
    controlador padrão é `Home` e a ação padrão é `Index`.
    
    Essa alteração ajusta o comportamento padrão da aplicação,
    redirecionando para `Home/Index` ao acessar sem especificar
    um controlador ou ação.

--- RecupereJa/Program.cs
+++ RecupereJa/Program.cs
@@ -46,7 +46,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Usuario}/{action=Login}/{id?}"
+    pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 app.Run();
