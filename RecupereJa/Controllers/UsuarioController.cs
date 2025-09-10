b64a92455fa8b8b8aacff77a04f034f29c3171e8
Maicon Gabriel Vanelli <maicongabrielvanelli@gmail.com>
2025-09-10 13:40:12 -0300

    Refatoração de layout e melhorias na experiência do usuário
    Alterações no redirecionamento após login em `UsuarioController.cs`, mudando de "Index" para "Home".
    
    Reformulação do layout em `Create.cshtml`, com novos estilos e validação aprimorada.
    
    Atualização de `Delete.cshtml` para incluir confirmação de exclusão com SweetAlert2.
    
    Melhorias na apresentação em `Details.cshtml`, incluindo cabeçalho e rodapé.
    
    Remoção temporária do rodapé em `_Layout.cshtml` para manter responsividade.
    
    Alterações significativas no layout de `Perfil.cshtml`, com novos estilos e cabeçalho fixo.
    
    Adição de alerta de sucesso em `Registrar.cshtml` após registro.
    
    Remoção do arquivo `Sucesso.cshtml`.
    
    Atualizações no CSS em `Perfil.css` para melhorar a apresentação.
    
    Vários arquivos de imagem foram adicionados.

--- RecupereJa/Controllers/UsuarioController.cs
+++ RecupereJa/Controllers/UsuarioController.cs
@@ -66,7 +66,7 @@ namespace RecupereJa.Controllers
             if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
                 return Redirect(returnUrl);
 
-            return RedirectToAction("Index", "Item");
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
